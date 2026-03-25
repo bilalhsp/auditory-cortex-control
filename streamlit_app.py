ed88fddd85ada2b4f95a2c3cbf4e62270e2ea4c8
Bilal Ahmed <bilalhsp@gmail.com>
2026-03-25 03:08:30 -0400

    cupy erros fixing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
-# streamlit_app.py
 import os
+import sys
+import glob
+
 os.environ.pop("LD_LIBRARY_PATH", None)
+
+# Put your conda env lib directory first
+conda_prefix = sys.prefix
+conda_lib = os.path.join(conda_prefix, "lib")
+if os.path.isdir(conda_lib):
+    os.environ["LD_LIBRARY_PATH"] = conda_lib
+
+# Preload conda's CUDA libs first
+for pattern in [
+    "libcublas.so*",
+    "libcusolver.so*",
+    "libcudart.so*",
+]:
+    for lib in sorted(glob.glob(os.path.join(conda_lib, pattern))):
+        try:
+            import ctypes
+            ctypes.CDLL(lib, mode=ctypes.RTLD_GLOBAL)
+        except OSError:
+            pass
 
 import streamlit as st
 from pathlib import Path
